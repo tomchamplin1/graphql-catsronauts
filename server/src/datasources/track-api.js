0017eec8847e3a9cd8608c6b41c85b5eccf257a0
Tom Champlin <64120886+tomchamplin1@users.noreply.github.com>
2022-09-27 17:44:23 -0500

    first commit

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -1,14 +1,18 @@
-const { RESTDataSource } = require('apollo-datasource-rest');
+const { RESTDataSource } = require("apollo-datasource-rest");
 
 class TrackAPI extends RESTDataSource {
   constructor() {
     super();
     // the Catstronauts catalog is hosted on this server
-    this.baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
+    this.baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
+  }
+
+  incrementTrackViews(trackId) {
+    return this.patch(`track/${trackId}/numberOfViews`);
   }
 
   getTracksForHome() {
-    return this.get('tracks');
+    return this.get("tracks");
   }
 
   getAuthor(authorId) {
